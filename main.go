5185f58154d83cb5d2c4ad6229662c3e7c91d62f
mirak <130295119+miraklik@users.noreply.github.com>
2024-05-02 20:03:50 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,21 +3,24 @@ package main
 import "fmt"
 
 func Spen(a int) string {
-	if a == 1 {
-		return string(a) + " компьютер"
+	var word string
+	switch a % 10 {
+	case 1:
+		return fmt.Sprintln(a, "компьютер")
+	case 2, 3, 4:
+		return fmt.Sprintln(a, "компьютера")
+	default:
+		return fmt.Sprintln(a, "компьютеров")
 	}
-	if a > 1 && a < 5 {
-		return string(a) + "компьютера"
+
+	if a/10%10 == 1 {
+		word = "компьютер"
 	}
-	if a > 4 && a < 41 {
-		return string(a) + "компьютеров"
-	}
-	if a >= 41 && a < 42 {
-		return string(a) + "компьютер"
-	}
-	return fmt.Sprintln(a, "компьютеров")
+
+	return fmt.Sprintln(a, word)
+
 }
 
 func main() {
-	fmt.Println(Spen(1024))
+	fmt.Println(Spen(2301))
 }
